5875ded7dbbc8f142b09bb58c911d1c796aede98
Uriel-200931 <uriel.maldonadocortez@utxicotepec.edu.mx>
2023-08-10 23:32:56 -0600

    Cambios Backend SP CLEAN AND FILL

--- Backend/NEARBY_B_2.0/config/db.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine, MetaData
-
-engine = create_engine("mysql+pymysql://root:1234@localhost:3306/ddi_nearby_b_db_2")
-
-meta = MetaData()
-
-conn = engine.connect()
